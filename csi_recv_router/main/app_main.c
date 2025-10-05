b286fcf625049ff0becc0c93a126e8568cbe295c
Ashwin A Nayar <ashwin5059198@gmail.com>
2025-10-05 19:29:53 +0530

    Copy csi_recv_router from espressif/esp-csi at 3726a94ccdb3984254a287292f69b2ba583b2a47

---
+++ csi_recv_router/main/app_main.c
@@ -0,0 +1,256 @@
+/* Get recv router csi
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "nvs_flash.h"
+
+#include "esp_mac.h"
+#include "rom/ets_sys.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "esp_now.h"
+
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
+#include "ping/ping_sock.h"
+
+#include "protocol_examples_common.h"
+
+#define CONFIG_SEND_FREQUENCY 100
+#if CONFIG_IDF_TARGET_ESP32C5
+#define CSI_FORCE_LLTF 1
+#endif
+#define CONFIG_FORCE_GAIN 1
+
+#if CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32C5 || CONFIG_IDF_TARGET_ESP32C6
+#define CONFIG_GAIN_CONTROL 1
+#endif
+
+static const char *TAG = "csi_recv_router";
+typedef struct
+{
+    unsigned : 32; /**< reserved */
+    unsigned : 32; /**< reserved */
+    unsigned : 32; /**< reserved */
+    unsigned : 32; /**< reserved */
+    unsigned : 32; /**< reserved */
+#if CONFIG_IDF_TARGET_ESP32S2
+    unsigned : 32; /**< reserved */
+#elif CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32C5 || CONFIG_IDF_TARGET_ESP32C6
+    unsigned : 16; /**< reserved */
+    unsigned fft_gain : 8;
+    unsigned agc_gain : 8;
+    unsigned : 32; /**< reserved */
+#endif
+    unsigned : 32; /**< reserved */
+#if CONFIG_IDF_TARGET_ESP32S2
+    signed : 8;    /**< reserved */
+    unsigned : 24; /**< reserved */
+#elif CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32C5
+    unsigned : 32; /**< reserved */
+    unsigned : 32; /**< reserved */
+    unsigned : 32; /**< reserved */
+#endif
+    unsigned : 32; /**< reserved */
+} wifi_pkt_rx_ctrl_phy_t;
+
+#if CONFIG_FORCE_GAIN
+/**
+ * @brief Enable/disable automatic fft gain control and set its value
+ * @param[in] force_en true to disable automatic fft gain control
+ * @param[in] force_value forced fft gain value
+ */
+extern void phy_fft_scale_force(bool force_en, uint8_t force_value);
+
+/**
+ * @brief Enable/disable automatic gain control and set its value
+ * @param[in] force_en true to disable automatic gain control
+ * @param[in] force_value forced gain value
+ */
+extern void phy_force_rx_gain(int force_en, int force_value);
+#endif
+
+static void wifi_csi_rx_cb(void *ctx, wifi_csi_info_t *info)
+{
+    if (!info || !info->buf)
+    {
+        ESP_LOGW(TAG, "<%s> wifi_csi_cb", esp_err_to_name(ESP_ERR_INVALID_ARG));
+        return;
+    }
+
+    if (memcmp(info->mac, ctx, 6))
+    {
+        return;
+    }
+
+    static int s_count = 0;
+    const wifi_pkt_rx_ctrl_t *rx_ctrl = &info->rx_ctrl;
+    wifi_pkt_rx_ctrl_phy_t *phy_info = (wifi_pkt_rx_ctrl_phy_t *)info;
+#if CONFIG_GAIN_CONTROL
+    static uint16_t agc_gain_sum = 0;
+    static uint16_t fft_gain_sum = 0;
+    static uint8_t agc_gain_force_value = 0;
+    static uint8_t fft_gain_force_value = 0;
+    if (s_count < 100)
+    {
+        agc_gain_sum += phy_info->agc_gain;
+        fft_gain_sum += phy_info->fft_gain;
+    }
+    else if (s_count == 100)
+    {
+        agc_gain_force_value = agc_gain_sum / 100;
+        fft_gain_force_value = fft_gain_sum / 100;
+#if CONFIG_FORCE_GAIN
+        phy_fft_scale_force(1, fft_gain_force_value);
+        phy_force_rx_gain(1, agc_gain_force_value);
+#endif
+        ESP_LOGI(TAG, "fft_force %d, agc_force %d", fft_gain_force_value, agc_gain_force_value);
+    }
+#endif
+
+#if CONFIG_IDF_TARGET_ESP32C5 || CONFIG_IDF_TARGET_ESP32C6
+    if (!s_count)
+    {
+        ESP_LOGI(TAG, "================ CSI RECV ================");
+        ets_printf("type,seq,mac,rssi,rate,noise_floor,fft_gain,agc_gain,channel,local_timestamp,sig_len,rx_state,len,first_word,data\n");
+    }
+    ets_printf("CSI_DATA,%d," MACSTR ",%d,%d,%d,%d,%d,%d,%d,%d,%d",
+               s_count, MAC2STR(info->mac), rx_ctrl->rssi, rx_ctrl->rate,
+               rx_ctrl->noise_floor, phy_info->fft_gain, phy_info->agc_gain, rx_ctrl->channel,
+               rx_ctrl->timestamp, rx_ctrl->sig_len, rx_ctrl->rx_state);
+#else
+    if (!s_count)
+    {
+        ESP_LOGI(TAG, "================ CSI RECV ================");
+        ets_printf("type,id,mac,rssi,rate,sig_mode,mcs,bandwidth,smoothing,not_sounding,aggregation,stbc,fec_coding,sgi,noise_floor,ampdu_cnt,channel,secondary_channel,local_timestamp,ant,sig_len,rx_state,len,first_word,data\n");
+    }
+    ets_printf("CSI_DATA,%d," MACSTR ",%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",
+               s_count, MAC2STR(info->mac), rx_ctrl->rssi, rx_ctrl->rate, rx_ctrl->sig_mode,
+               rx_ctrl->mcs, rx_ctrl->cwb, rx_ctrl->smoothing, rx_ctrl->not_sounding,
+               rx_ctrl->aggregation, rx_ctrl->stbc, rx_ctrl->fec_coding, rx_ctrl->sgi,
+               rx_ctrl->noise_floor, rx_ctrl->ampdu_cnt, rx_ctrl->channel, rx_ctrl->secondary_channel,
+               rx_ctrl->timestamp, rx_ctrl->ant, rx_ctrl->sig_len, rx_ctrl->rx_state);
+#endif
+
+#if CONFIG_IDF_TARGET_ESP32C5 && CSI_FORCE_LLTF
+    ets_printf(",%d,%d,\"[%d", (info->len - 2) / 2, info->first_word_invalid, (int16_t)(((int16_t)info->buf[1]) << 12) >> 4 | (uint8_t)info->buf[0]);
+    for (int i = 2; i < (info->len - 2); i += 2)
+    {
+        ets_printf(",%d", (int16_t)(((int16_t)info->buf[i + 1]) << 12) >> 4 | (uint8_t)info->buf[i]);
+    }
+#else
+    ets_printf(",%d,%d,\"[%d", info->len, info->first_word_invalid, info->buf[0]);
+    for (int i = 1; i < info->len; i++)
+    {
+        ets_printf(",%d", info->buf[i]);
+    }
+#endif
+    ets_printf("]\"\n");
+    s_count++;
+}
+
+static void wifi_csi_init()
+{
+    /**
+     * @brief In order to ensure the compatibility of routers, only LLTF sub-carriers are selected.
+     */
+#if CONFIG_IDF_TARGET_ESP32C5
+    wifi_csi_config_t csi_config = {
+        .enable = true,
+        .acquire_csi_legacy = true,
+        .acquire_csi_force_lltf = CSI_FORCE_LLTF,
+        .acquire_csi_ht20 = true,
+        .acquire_csi_ht40 = true,
+        .acquire_csi_vht = false,
+        .acquire_csi_su = false,
+        .acquire_csi_mu = false,
+        .acquire_csi_dcm = false,
+        .acquire_csi_beamformed = false,
+        .acquire_csi_he_stbc_mode = 2,
+        .val_scale_cfg = 0,
+        .dump_ack_en = false,
+        .reserved = false};
+#elif CONFIG_IDF_TARGET_ESP32C6
+    wifi_csi_config_t csi_config = {
+        .enable = true,
+        .acquire_csi_legacy = true,
+        .acquire_csi_ht20 = true,
+        .acquire_csi_ht40 = true,
+        .acquire_csi_su = false,
+        .acquire_csi_mu = false,
+        .acquire_csi_dcm = false,
+        .acquire_csi_beamformed = false,
+        .acquire_csi_he_stbc = 2,
+        .val_scale_cfg = false,
+        .dump_ack_en = false,
+        .reserved = false};
+#else
+    wifi_csi_config_t csi_config = {
+        .lltf_en = true,
+        .htltf_en = false,
+        .stbc_htltf2_en = false,
+        .ltf_merge_en = true,
+        .channel_filter_en = true,
+        .manu_scale = true,
+        .shift = true,
+    };
+#endif
+    static wifi_ap_record_t s_ap_info = {0};
+    ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&s_ap_info));
+    ESP_ERROR_CHECK(esp_wifi_set_csi_config(&csi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_csi_rx_cb(wifi_csi_rx_cb, s_ap_info.bssid));
+    ESP_ERROR_CHECK(esp_wifi_set_csi(true));
+}
+
+static esp_err_t wifi_ping_router_start()
+{
+    static esp_ping_handle_t ping_handle = NULL;
+
+    esp_ping_config_t ping_config = ESP_PING_DEFAULT_CONFIG();
+    ping_config.count = 0;
+    ping_config.interval_ms = 1000 / CONFIG_SEND_FREQUENCY;
+    ping_config.task_stack_size = 3072;
+    ping_config.data_size = 1;
+
+    esp_netif_ip_info_t local_ip;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &local_ip);
+    ESP_LOGI(TAG, "got ip:" IPSTR ", gw: " IPSTR, IP2STR(&local_ip.ip), IP2STR(&local_ip.gw));
+    ping_config.target_addr.u_addr.ip4.addr = ip4_addr_get_u32(&local_ip.gw);
+    ping_config.target_addr.type = ESP_IPADDR_TYPE_V4;
+
+    esp_ping_callbacks_t cbs = {0};
+    esp_ping_new_session(&ping_config, &cbs, &ping_handle);
+    esp_ping_start(ping_handle);
+
+    return ESP_OK;
+}
+
+void app_main()
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /**
+     * @brief This helper function configures Wi-Fi, as selected in menuconfig.
+     *        Read "Establishing Wi-Fi Connection" section in esp-idf/examples/protocols/README.md
+     *        for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    wifi_csi_init();
+    wifi_ping_router_start();
+}
